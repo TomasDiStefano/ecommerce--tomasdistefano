300a9481337551ee8f6b3e9de8f37afd67cf3d26
tDistefano <tomas.distefano98@gmail.com>
2023-11-08 19:24:11 -0300

    Add firestore instalation

---
+++ src/components/services/config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDw0fAOmeT5nfPY99G7PJ3o2ImOGzXDOd0",
+  authDomain: "ecommerce-diesel-juncal.firebaseapp.com",
+  projectId: "ecommerce-diesel-juncal",
+  storageBucket: "ecommerce-diesel-juncal.appspot.com",
+  messagingSenderId: "730217473515",
+  appId: "1:730217473515:web:51f8bf4b1f74bebc1c65ae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
